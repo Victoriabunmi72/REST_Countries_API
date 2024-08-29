4cc28d8c28e5abc5f99e0d5382646d10f28e0aaf
Victoriabunmi72 <bhummhie97@gmail.com>
2024-08-29 11:44:03 +0100

    Map_Explorer

--- rest-countries-map/src/App.js
+++ rest-countries-map/src/App.js
@@ -1,9 +1,9 @@
-// src/App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
+import L from 'leaflet';
 
 const continents = {
   Africa: 'Africa',
@@ -12,6 +12,13 @@ const continents = {
   Europe: 'Europe',
   Oceania: 'Oceania'
 };
+
+const customIcon = L.icon({
+  iconUrl: '../map_mk.png', // Replace  the path to your custom icon image
+  iconSize: [25, 41], // Size of the icon [width, height]
+  iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+  popupAnchor: [0, -41], // Point from which the popup should open relative to the iconAnchor
+});
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -46,6 +53,10 @@ function App() {
     }
   };
 
+  const handleCountryClick = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
     <div className="app">
       <h1>Country Explorer</h1>
@@ -67,35 +78,41 @@ function App() {
         <button type="submit">Search</button>
       </form>
 
-      <div className="country-list">
+      { selectedCountry === null && <div className="country-list">
         {filteredCountries.map(country => (
           <div
             key={country.cca3}
-            className="country-item"
-            onClick={() => setSelectedCountry(country)}
+            className={`country-item ${selectedCountry?.cca3 === country.cca3 ? 'active' : ''}`}
+            onClick={() => handleCountryClick(country)}
           >
             <h2>{country.name.common}</h2>
             <p>{country.capital && `Capital: ${country.capital[0]}`}</p>
           </div>
         ))}
-      </div>
+      </div>}
 
       {selectedCountry && (
-        <MapContainer
-          center={[selectedCountry.latlng[0], selectedCountry.latlng[1]]}
-          zoom={4}
-          scrollWheelZoom={false}
-          className="map"
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[selectedCountry.latlng[0], selectedCountry.latlng[1]]}>
-            <Popup>
-              {selectedCountry.name.common}
-            </Popup>
-          </Marker>
-        </MapContainer>
+        <div className="country-details">
+          <h2>{selectedCountry.name.common}</h2>
+          <p>Capital: {selectedCountry.capital && selectedCountry.capital[0]}</p>
+          <MapContainer
+            center={[selectedCountry.latlng[0], selectedCountry.latlng[1]]}
+            zoom={4}
+            scrollWheelZoom={false}
+            className="map"
+            
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              
+            />
+            <Marker position={[selectedCountry.latlng[0], selectedCountry.latlng[1]]} icon={customIcon}>
+              <Popup>
+                {selectedCountry.name.common}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       )}
     </div>
   );
